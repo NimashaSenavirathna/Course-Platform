001ad6b0d5296f5173f4a7777ff22244a6f783b7
DuminduSachintha <it22299284@my.sliit.lk>
2025-05-07 16:42:37 +0530

    Update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,29 @@ import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 import Success from './components/Success';
 import AdminPage from './components/AdminPage';
+import React, { useEffect, useState } from 'react';
+import { getPosts, createPost, deletePost } from './services/api';
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts().then(res => setPosts(res.data));
+  }, []);
+
+  const handleAdd = (post) => {
+    createPost(post).then(res => {
+      setPosts([res.data, ...posts]);
+    });
+  };
+
+  const handleDelete = (id) => {
+    deletePost(id).then(() => {
+      setPosts(posts.filter(post => post.id !== id));
+    });
+  };
 
 function App() {
   return (
@@ -20,7 +43,13 @@ function App() {
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
+      <div style={{ maxWidth: '600px', margin: 'auto' }}>
+        <h2>      </h2>
+        <PostForm onAdd={handleAdd} />
+        <PostList posts={posts} onDelete={handleDelete} />
+      </div>
   );
 }
+};
 
 export default App;
