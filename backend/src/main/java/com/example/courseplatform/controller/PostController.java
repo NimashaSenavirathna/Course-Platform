fdf6ec79e17e9a824b1d4017099ae37d0187d2fc
RavinduLakshitha07 <ravindulakshitha121@gmail.com>
2025-05-07 18:48:58 +0530

    Backend

---
+++ backend/src/main/java/com/example/courseplatform/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.example.socialmedia.controller;
+
+import com.example.socialmedia.model.Post;
+import com.example.socialmedia.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+@CrossOrigin(origins = "*")
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable String id) {
+        return postRepository.findById(id).orElse(null);
+    }
+}
