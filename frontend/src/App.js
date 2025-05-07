cbea3010b9a4797b2f809c4fced5b9693a9f2c8a
DuminduSachintha <it22299284@my.sliit.lk>
2025-05-07 16:43:32 +0530

    aaa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import CourseList from './components/CourseList';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout';
-import Success from './components/Success';
-import AdminPage from './components/AdminPage';
 import React, { useEffect, useState } from 'react';
 import { getPosts, createPost, deletePost } from './services/api';
 import PostForm from './components/PostForm';
@@ -30,26 +22,14 @@ const App = () => {
     });
   };
 
-function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<CourseList />} />
-        <Route path="/courses" element={<CourseList />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
-    </Router>
       <div style={{ maxWidth: '600px', margin: 'auto' }}>
         <h2>      </h2>
         <PostForm onAdd={handleAdd} />
         <PostList posts={posts} onDelete={handleDelete} />
       </div>
   );
-}
 };
 
 export default App;
