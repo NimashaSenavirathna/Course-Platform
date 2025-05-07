db57663858694bd9fa9cfb242fbda9486abb284a
Miyuru Sadanjith Wijesinghe <144732464+MiyuRuu22@users.noreply.github.com>
2025-05-07 21:30:07 +0530

    Create Home.js
    Home page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1 className="text-2xl font-bold">Welcome to Learning Plans</h1>
+    <p>Use the navigation above to get started.</p>
+  </div>
+);
+
+export default Home;
